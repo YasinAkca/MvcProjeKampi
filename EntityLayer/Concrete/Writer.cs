523a5852f7425df532f71faa2f3e215b1f56c7a8
YasinAkca <yasinakcaa1@outlook.com>
2023-07-25 10:54:19 +0300

    53. video

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -15,7 +15,7 @@ namespace EntityLayer.Concrete
         public string WriterName { get; set; }
         [StringLength(50)]
         public string WriterSurName { get; set; }
-        [StringLength(100)]
+        [StringLength(250)]
         public string WriterImage{ get; set; }
         [StringLength(100)]
         public string WriterAbout { get; set; }
@@ -24,7 +24,8 @@ namespace EntityLayer.Concrete
         [StringLength(200)]
         public string WriterPassword { get; set; }
         [StringLength(50)]
-        public string WriterTitle { get; set; }
+        public string WriterTitle { get; set; }        
+        public bool WriterStatus { get; set; }
 
 
         public ICollection<Heading> Headings { get; set; }
---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryMenager cm = new CategoryMenager(new EfCategoryDal());
+        WriterMenager wm = new WriterMenager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList() select new SelectListItem { Text=x.CategoryName,Value=x.CategoryID.ToString()}).ToList();
+            ViewBag.vlc = valuecategory;
+            List<SelectListItem> valuewriter = (from x in wm.GetList() select new SelectListItem { Text = x.WriterName, Value = x.YazarID.ToString() }).ToList();
+            ViewBag.vlw = valuewriter;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ContentByHeading()
+        {
+            return View();
+        }
+    }
+}
