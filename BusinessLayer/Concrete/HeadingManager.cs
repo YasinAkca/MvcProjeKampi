8b26b08f4c4379e86fb84991c2320d560783715a
YasinAkca <yasinakcaa1@outlook.com>
2023-07-31 15:18:26 +0300

    73. video bitti/WriterHeadingAdded

--- BusinessLayer/Concrete/HeadingManager.cs
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -49,5 +49,10 @@ namespace BusinessLayer.Concrete
         {
             _headingDal.Update(heading);
         }
+
+        public List<Heading> GetListByWriter(int id)
+        {
+            return _headingDal.List(x => x.WriterID == id);
+        }
     }
 }
--- MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,16 @@ namespace MvcProjeKampi.Controllers
 {
     public class WriterPanelController : Controller
     {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
         // GET: WriterPanel
         public ActionResult WriterProfile()
         {
+            return View();
+        }
+        public ActionResult MyHeading(int id)
+        {
+            id = 4;
+            var values = hm.GetListByWriter(id);
             return View();
         }
     }
